1cea1c7e509f574e44621785d176ac1a6e94ca36
LawrenceLevel-FS <141098091+LawrenceLevel-FS@users.noreply.github.com>
2024-07-04 20:58:09 -0400

    test commit

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST='{"pages":{},"app":{"/Users/levellawrence/Desktop/Desktop - Level’s MacBook Pro/Fullsail Univerity/deploymentOfWebApp/week1/nextdemo/app/layout":["static/media/c9a5bc6a7c948fb0-s.p.woff2"]},"appUsingSizeAdjust":true,"pagesUsingSizeAdjust":false}';
---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"KMIvC2EWqcUgsnhkPfegwLLZlg5lElZuM20GuvF15NU=\"}"
